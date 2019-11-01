b0d1e6c15daba0e23d347f24d0e62aeaaa180a05
princeiiitd <prince18073@iiitd.ac.in>
2019-11-02 03:42:09 +0530

    2nov-03:40

--- sample/Main.java
+++ sample/Main.java
@@ -10,11 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("page1.fxml"));
+        primaryStage.setTitle("Plant_V_Zombies");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
