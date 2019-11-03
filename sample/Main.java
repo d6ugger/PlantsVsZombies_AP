af46254796455d2408879f8bcb8939647af898df
princeiiitd <prince18073@iiitd.ac.in>
2019-11-03 14:33:08 +0530

    3nov-14:32

--- sample/Main.java
+++ sample/Main.java
@@ -12,10 +12,20 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("page1.fxml"));
         primaryStage.setTitle("Plant_V_Zombies");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+        func2(primaryStage);
+
     }
 
+    public void func2( Stage stage2 ) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("page2.fxml"));
+        stage2.setTitle("Game Page");
+        stage2.setScene(new Scene(root, 600,400));
+        stage2.show();
+
+
+        }
 
 
     public static void main(String[] args) {
